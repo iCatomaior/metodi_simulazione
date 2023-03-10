5cd76ba0ded3b5f10407edfdc82f37e5e3789389
iCatomaior <matticre@protonmail.com>
2023-03-10 12:44:08 +0100

    Updates

--- metropolis/metropolis.cpp
+++ metropolis/metropolis.cpp
@@ -2,12 +2,16 @@
 #include "TMath.h"
 #include "TApplication.h"
 #include "TRandom3.h"
+#include "TCanvas.h"
 #include "TGraph.h"
 
 using namespace std;
 
 //parametri fisici del problema
-double J = 5;
+double J=3;
+
+//generatore
+TRandom3 rnd(123456789);
 
 //checking the number of particles
 void check(int** matrix, int size){
@@ -29,24 +33,63 @@ if (N_acc!=N_particles){
 }
 
 double Energy(int **matrix, int L, int x, int y){
+    
     double energy;
-    energy = J*(matrix[(x+1)%L][y]+matrix[abs(x-1)%L][y]+matrix[x][(y+1)%L]+matrix[x][abs(y-1)%L]);
+    energy = J*(matrix[(x+1+L)%L][y]+matrix[(x-1+L)%L][y]+matrix[x][(y+1+L)%L]+matrix[x][(y-1+L)%L]);
     return energy;
 }
+
+
+void Evolve (int **matrix, int L, int x, int y){
+        double E_i, E_f, delta, p, n;
+
+        int x_f = x;
+        int y_f = y;
+        E_i = Energy(matrix,L,x,y);
+        n = rnd.Rndm();
+
+        if (n<0.25){
+            y_f=(y+L+1)%L;
+
+        } else if (n<0.5){
+            y_f=(y-1+L)%L;
+        
+        } else if (n<0.75){
+            x_f=(x+1+L)%L;
+        
+        } else {
+            x_f=(x-1+L)%L;
+        
+        }
+
+        if (matrix[x_f][y_f]==0){
+        
+            E_f = Energy(matrix, L, x_f, y_f);
+            p   = TMath::Exp(E_i-E_f);
+            n   = rnd.Rndm();
+
+            if (E_f>E_i){
+                if(n<p){
+                    matrix[x][y]=0;
+                    matrix[x_f][y_f]=1;
+                }
+            
+            } else {
+                matrix[x][y]=0;
+                matrix[x_f][y_f]=1;
+
+            }
+        
+        }
+    }
+
 
 int main(){
 
     TApplication app("app",NULL,NULL);
 
-//inizializzazione generatore tra 0 e 1
-    TRandom3 rnd;
-    rnd.SetSeed(123456789);
-
-//larghezza griglia
-    const int L = 16;
-
-//parametri
-    int N_particles = L*L/2;
+    const int L = 16;           //larghezza griglia
+    int N_particles = L*L/2;    //numero di particelle
 
 //inizializzazione griglia
     int **matrix = new int*[L];
@@ -62,18 +105,15 @@ int main(){
         }
     }
 
-// variabili coordinate
-    int x,y;
-
-//variabile contatore
-    int N_acc = 0;
+    int x,y;        // variabili coordinate
+    int N_acc = 0;  // variabile contatore
 
 //riempimento griglia
     while(N_acc<N_particles) {
         x=rnd.Integer(L);
         y=rnd.Integer(L);
 
-        if (matrix[x][y]==0){
+        if (matrix[x][y] == 0){
             matrix[x][y] = 1;
             N_acc++;
         }
@@ -82,52 +122,22 @@ int main(){
 //checking matrix
     check(matrix,L);
 
-//numero di step di montecarlo
 
-int NMC = 10000;
+int NMC = 1000;    //numero di step di montecarlo
 
-for (int i=0; i<NMC*N_particles; i++){
-    x = rnd.Integer(L);
-    y = rnd.Integer(L);
+for (int i=0; i<NMC; i++){
+    for (int j=0; j<N_particles; j++){
+        x = rnd.Integer(L);
+        y = rnd.Integer(L);
 
-    if(matrix[x][y]==0){
-        continue;
-    
-    } else{
-        double E_i, E_f, delta, p, n;
-
-        int x_f = x;
-        int y_f = y;
-        E_i = Energy(matrix,L,x,y);
-        n = rnd.Rndm();
-
-        if (n<0.25){
-            y_f=abs(y+1)%L;
-
-        } else if (n<0.5){
-            y_f=abs(y-1)%L;
-        
-        } else if (n<0.75){
-            x_f=abs(x+1)%L;
-        
-        } else {
-            x_f=abs(x-1)%L;
-        
+        if(matrix[x][y]==1){
+            Evolve(matrix,L,x,y); 
         }
-
-        if (matrix[x_f][y_f]==0){
-        
-            E_f = Energy(matrix, L, x_f, y_f);
-            p   = TMath::Exp(E_i-E_f);
-            n   = rnd.Rndm();
-
-            if(n<p){
-                matrix[x][y]=0;
-                matrix[x_f][y_f]=1;
-            }
-        }
-    }
+    }   
 }
+
+//checking matrix
+    check(matrix,L);
 
 //plotting
     
@@ -135,7 +145,6 @@ for (int i=0; i<NMC*N_particles; i++){
     TGraph* g2 = new TGraph();
 
     int marker_size = 1;
-
 
     g1->SetMarkerStyle(20);
     g1->SetMarkerSize(marker_size);
@@ -160,7 +169,6 @@ for (int i=0; i<NMC*N_particles; i++){
         }
     }
 
-
     g2->Draw("AP");
     g1->Draw("P");
 
@@ -176,4 +184,3 @@ for (int i=0; i<NMC*N_particles; i++){
 
     return 0;
 }
-
